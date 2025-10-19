9dc54243fed07af68fed13aac672cbc7c085024f
oleksandr-drohashevskyi <alexandr.drogashevskij@gmail.com>
2025-10-19 22:10:01 +0200

    Hometask3 (#3)
    * added library
    
    * initial com
    
    * hometask3 solutions
    
    * Update .gitignore
    
    * Update .gitignore
    
    * Update .gitignore2
    
    * deleted folders
    
    * Update .gitignore to exclude node_modules and practice
    
    * updated git ignore

---
+++ jsBasics/task2.js
@@ -0,0 +1,9 @@
+let firstPerson = "Donald Tramp";
+let secondPerson = "Xi Jingping";
+let greeting ="Hello, " + firstPerson + " and " + secondPerson+"!";
+console.log( greeting);
+
+
+let greetingTemplate = `Hello, ${firstPerson} and ${secondPerson}! Don't apply new tariffs please,
+we want TON back to 3$!`;
+console.log(greetingTemplate);
---
+++ jsBasics/task4.1.js
@@ -0,0 +1,5 @@
+let radius = 10;
+const PI=Math.PI;
+let area = PI* (radius**2);
+
+console.log(Number(area.toFixed(2)));
