91628f2b46c04571462678088d5429924adedfd3
oleksadnr-drohashevskyi <alexandr.drogashevskij@gmail.com>
2025-10-20 13:13:29 +0200

    added task3

---
+++ homework les 5/task3.js
@@ -0,0 +1,12 @@
+const x =3;
+
+for (let i=1; i <=10; i++){
+    console.log(x, " * ", i, " = ", x*i)
+}
+////////
+
+let y = 1;
+while (y <=10){
+    console.log(x, " * ", y, " = ", x*y);
+    y++;
+}
