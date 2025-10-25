77d1339d515df652660ced9f547c764b22130a11
oleksadnr-drohashevskyi <alexandr.drogashevskij@gmail.com>
2025-10-26 00:20:21 +0200

    added homework 6

---
+++ homework les 6/task1.js
@@ -0,0 +1,14 @@
+// function area (width, height) {
+//     console.log("Площа прямокутника = " , width*height)
+// }
+// area(2,10)
+
+
+// const area = function(width, height){
+//     return width*height;
+// }
+// console.log(area(2,5));
+
+
+const area = (width, height) => console.log(`Площа прямокутника ${width*height}`)
+area (3,9)
