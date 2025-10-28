32d25ddc0dfb099e7f92638b9da1d88191a7d7ee
oleksandr-drohashevskyi <alexandr.drogashevskij@gmail.com>
2025-10-28 23:00:09 +0100

    added homework les 8 (#7)

---
+++ homework les 8/task5.js
@@ -0,0 +1,11 @@
+// const firstArray = [1, 1, 1, 1];
+// const secondArray= [0, 0, 0, 0];
+
+// const merged= firstArray.concat(secondArray);
+// console.log(merged);
+
+const firstArray = [1, 1, 1, 1];
+const secondArray= [0, 0, 0, 0];
+
+const merged= [...firstArray, ...secondArray];
+console.log(merged);
