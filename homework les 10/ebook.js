5b1cce978634b749040630dd5ef3c2a024d810cf
oleksandr-drohashevskyi <alexandr.drogashevskij@gmail.com>
2025-11-09 21:29:38 +0100

    added homework les 10 (#9)

---
+++ homework les 10/ebook.js
@@ -0,0 +1,33 @@
+import { Book } from "./book.js";
+
+export class EBook extends Book {
+  constructor(title, author, year, fileFormat) {
+    super(title, author, year);
+    this.fileFormat = fileFormat; 
+  }
+
+  get fileFormat() {
+    return this._fileFormat;
+  }
+  set fileFormat(value) {
+    if (typeof value !== "string" || value.trim() === "") {
+      throw new Error("fileFormat має бути непорожнім рядком");
+    }
+    this._fileFormat = value.trim(); 
+  }
+
+
+  printInfo() {
+    console.log(
+      `Назва: ${this.title}, Автор: ${this.author}, Рік: ${this.year}, Формат: ${this.fileFormat}`
+    );
+  }
+
+  
+  static fromBook(book, format) {
+    if (!(book instanceof Book)) {
+      throw new Error("Перший аргумент має бути екземпляром Book");
+    }
+    return new EBook(book.title, book.author, book.year, format);
+  }
+}
---
+++ homework les 10/task1.js
@@ -0,0 +1,32 @@
+import { Book } from "./book.js";
+import { EBook } from "./ebook.js";
+
+
+const b1 = new Book("Майстер і Маргарита", "М. Булгаков", 1967);
+const b2 = new Book("1984", "Джордж Орвелл", 1949);
+const b3 = new Book("Гаррі Поттер і філософський камінь", "Дж. К. Ролінг", 1997);
+
+
+b1.printInfo();
+b2.printInfo();
+b3.printInfo();
+
+
+const eb1 = new EBook("Clean Code", "Robert C. Martin", 2008, "PDF");
+eb1.printInfo();
+
+
+const eb2 = EBook.fromBook(b2, "EPUB");
+eb2.printInfo();
+
+
+eb1.title = "Clean Code (Updated)";
+eb1.year = 2010;
+eb1.fileFormat = "MOBI";
+eb1.printInfo();
+
+
+const all = [b1, b2, b3, eb1, eb2];
+const oldest = Book.oldest(all);
+console.log("Найдавніша книга:");
+oldest.printInfo();
